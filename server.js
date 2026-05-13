c0ce5bda590b842bbf2f569ddff2c0be7a4b09fc
ConzTheCodeWizzard <conzbeatz01@gmail.com>
2026-05-13 18:45:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,44 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Conz Bot Online");
+
+    res.send(`
+    <html>
+
+    <body style="
+    background:black;
+    color:lime;
+    font-family:monospace;
+    text-align:center;
+    padding-top:100px;
+    ">
+
+    <h1>CONZ BOT ONLINE</h1>
+
+    <p>Bot server is running.</p>
+
+    </body>
+    </html>
+    `);
+
 });
 
 app.post("/incoming", (req, res) => {
 
-    console.log(req.body);
+    const message = req.body.message;
+
+    console.log("Message:", message);
+
+    if(message === "!ping"){
+        console.log("Pong command used");
+    }
+
+    if(message === "!hello"){
+        console.log("Hello command used");
+    }
 
     res.sendStatus(200);
+
 });
 
 app.listen(3000, () => {
